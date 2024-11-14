c1dee0291fc30161bbe5d17536e28d1c99aa6836
Jun Kimura <jun.kimura@datachain.jp>
2024-11-14 10:28:11 +0900

    improve tests
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- crates/consensus/src/fork/electra.rs
+++ crates/consensus/src/fork/electra.rs
@@ -7,3 +7,11 @@ pub const ELECTRA_FORK_SPEC: ForkSpec = ForkSpec {
     next_sync_committee_gindex: 87,
     ..deneb::DENEB_FORK_SPEC
 };
+
+pub type ExecutionPayloadHeader<
+    const BYTES_PER_LOGS_BLOOM: usize,
+    const MAX_EXTRA_DATA_BYTES: usize,
+> = deneb::ExecutionPayloadHeader<BYTES_PER_LOGS_BLOOM, MAX_EXTRA_DATA_BYTES>;
+
+pub type LightClientHeader<const BYTES_PER_LOGS_BLOOM: usize, const MAX_EXTRA_DATA_BYTES: usize> =
+    deneb::LightClientHeader<BYTES_PER_LOGS_BLOOM, MAX_EXTRA_DATA_BYTES>;
